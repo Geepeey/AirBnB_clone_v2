1626d122ac61d211dcbbe624a2c62a8b224ca76f
Godspower Udokpoh <harshmallins@gmail.com>
2023-04-21 06:03:37 -0700

    Number Temp

--- web_flask/5-number_template.py
+++ web_flask/5-number_template.py
@@ -18,7 +18,7 @@ def hello_hbnb():
 
 @app.route('/hbnb', strict_slashes=False)
 def hbnb():
-     """
+    """
     Displays "HBNB"
     """
     return 'HBNB'
@@ -27,16 +27,16 @@ def hbnb():
 @app.route('/c/<text>', strict_slashes=False)
 def c(text):
     """
-    Route display “C ”, followed by the value of the text variable.
+    Route displays "C", followed by the value of the text variable.
     """
     return 'C {}'.format(text.replace('_', ' '))
 
 
 @app.route('/python/', defaults={'text': 'is cool'}, strict_slashes=False)
 @app.route('/python/<text>', strict_slashes=False)
-def python(text):
+def python(text='is cool'):
     """
-    Route display “Python ”, followed by the value of the text variable.
+    Route displays "Python", followed by the value of the text variable.
     """
     return 'Python {}'.format(text.replace('_', ' '))
 
@@ -44,7 +44,7 @@ def python(text):
 @app.route('/number/<int:n>', strict_slashes=False)
 def number(n):
     """
-    Route display “n is a number” only if n is an integer.
+    Route displays “n is a number” only if n is an integer.
     """
     return '{} is a number'.format(n)
 
@@ -52,7 +52,7 @@ def number(n):
 @app.route('/number_template/<int:n>', strict_slashes=False)
 def number_template(n):
     """
-    Route display a HTML page only if n is an integer.
+    Route displays a HTML page only if n is an integer.
     """
     return render_template('5-number.html', n=n)
 
