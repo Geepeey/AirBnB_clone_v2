d0904c6212c8820a9fa1067ccd4814437b031edb
Geepeey <111070276+Geepeey@users.noreply.github.com>
2023-04-24 19:19:58 +0100

    7-states_list.py

--- web_flask/7-states_list.py
+++ web_flask/7-states_list.py
@@ -1,10 +1,15 @@
 #!/usr/bin/python3
-"""This module starts a Flask web application that displays a list of states."""
+"""
+Starts a Flask web application
+The application listens on 0.0.0.0, port 5000.
+Routes:
+    /states: HTML page with a list of all State objects.
+    /states/<id>: HTML page displaying the given state with <id>.
+"""
 
 from flask import Flask, render_template
 from models import storage
 from models.state import State
-
 
 app = Flask(__name__)
 
@@ -18,7 +23,7 @@ def teardown_session(exception):
 @app.route('/states_list', strict_slashes=False)
 def states_list():
     """Displays a list of states."""
-    states = storage.all(State)
+    states = storage.all(State).values()
     return render_template('7-states_list.html', states=states)
 
 
